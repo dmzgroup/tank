8974dfc8cf310ab8f74d21a4ca6b317ad6da4715
Randall E. Barker <simstorm@mac.com>
2009-02-18 13:29:02 -0800

    added default when running

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 
 . ../scripts/envsetup.sh
 
-$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/runtime.xml config/resource.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/render.xml config/tank.xml config/lua.xml $*
+if [ "$1" = "" ] ; then
+   export EXTRA_FILE=config/blue.xml ;
+fi
+
+$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/runtime.xml config/resource.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/render.xml config/tank.xml config/lua.xml $* $EXTRA_FILE
